89824d0950087e3ccd7157bb4bc728a14220986b
X360pro <43405739+X360pro@users.noreply.github.com>
2020-05-04 13:21:05 +0530

    Add files via upload

---
+++ taskstruct.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstname string
+	lastname string
+	age int
+	address []string
+}
+
+func main(){
+	var person1 person
+	person1.firstname = "abhishek"
+	person1.lastname = "nagvekar"
+	person1.age = 20
+	person1.address = make([]string,2)
+	person1.address[0] = "andheri"
+	fmt.Println(person1)
+}
