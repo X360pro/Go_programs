373dfdfc5a73cb4a389563ba34985cee591a88fa
X360pro <43405739+X360pro@users.noreply.github.com>
2020-05-04 13:29:44 +0530

    Add files via upload

--- taskstruct.go
+++ taskstruct.go
@@ -11,10 +11,17 @@ type person struct {
 
 func main(){
 	var person1 person
+	var person2 person
 	person1.firstname = "abhishek"
 	person1.lastname = "nagvekar"
 	person1.age = 20
-	person1.address = make([]string,2)
+	person1.address = make([]string,1)
 	person1.address[0] = "andheri"
 	fmt.Println(person1)
+	person2.firstname = "soham"
+	person2.lastname = "nagvekar"
+	person2.age = 16
+	person2.address = make([]string,1)
+	person2.address[0] = "andheri"
+	fmt.Println(person2)
 }
