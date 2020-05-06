2ef1bd8cb19894801d8c56835b9e587d21e3d2d7
X360pro <43405739+X360pro@users.noreply.github.com>
2020-05-06 13:52:57 +0530

    Update taskstruct2.go

--- taskstruct2.go
+++ taskstruct2.go
@@ -17,6 +17,19 @@ func assign_values(curPerson person, first, last string, curAge int8, curAdd []s
 	return curPerson
 }
 
+func modifyPointer(curPerson *person) {
+	curPerson.firstname = "Abhishek"
+	curPerson.lastname = "Nagvekar"
+	curPerson.age = 20
+}
+
+func modifyValue(curPerson person) person {
+	curPerson.firstname = "Soham"
+	curPerson.lastname = "Nagvekar"
+	curPerson.age = 17
+	return curPerson
+}
+
 func printlnfo(curPerson *person) {
 	fmt.Println("First Name : ", curPerson.firstname)
 	fmt.Println("Last Name : ", curPerson.lastname)
@@ -36,5 +49,8 @@ func main() {
 	var person1 person
 	addresses := []string{"usa", "india", "uk", "canada"}
 	person1 = assign_values(person1, "john", "lark", 46, addresses)
-	fmt.Println(person1.getFullName())
+	modifyPointer(&person1)
+	newPerson := modifyValue(person1)
+	fmt.Println("Person modified by pointer : ", person1)
+	fmt.Println("Person modified by value : ", newPerson)
 }
