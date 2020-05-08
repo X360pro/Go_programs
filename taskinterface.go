2d5de02db77b370e060baabd8802cc44327f6d09
X360pro <43405739+X360pro@users.noreply.github.com>
2020-05-08 13:21:17 +0530

    Update taskinterface.go

--- taskinterface.go
+++ taskinterface.go
@@ -11,17 +11,17 @@ type square struct {
 	side float64
 }
 
-func (s square) perimeter() float64 {
+func (s *square) perimeter() float64 {
 	return (4 * s.side)
 }
 
-func (s square) area() float64 {
+func (s *square) area() float64 {
 	return (s.side * s.side)
 }
 
 func main() {
 	var newSquare shape
-	newSquare = square{5.0}
+	newSquare = &square{5.0}
 	fmt.Println("Perimeter of square is ", newSquare.perimeter())
 	fmt.Println("Area is ", newSquare.area())
 }
